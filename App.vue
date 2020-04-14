80a5151bd8b7762961c793d92626f2b7bbc19192
Storh <storh1118@outlook.com>
2020-04-14 11:40:23 +0800

    函数位置bug

--- App.vue
+++ App.vue
@@ -14,15 +14,14 @@
 		methods: {
 			// 验证是否登录过 登录超时在发送信息时验证
 			checkLogin() {
+				const token = uni.getStorageSync('webToken');
 				const userInfo = uni.getStorageSync('userData');
-				if (userInfo) {
+				if (userInfo&&token) {
 					// 必须用户曾经的信息和token都存在才算用户已登录过
 					User.getInfo(userInfo.user_id).then(res => {
 						uni.setStorageSync('userData', res.data.data);
 						// 是否已经添加小区信息
-						if (userInfo.districts.estate.id) {
-
-						} else {
+						if (!userInfo.districts.estate.id) {
 							uni.showModal({
 								title: '信息不完整',
 								content: '信息不完整，请先补充信息',
@@ -47,30 +46,29 @@
 					// 用户从未登录或清理缓存
 					this.toLogin();
 				}
+			}, // 获取当前页面路径并跳转去登录
+			toLogin() {
+				uni.showModal({
+					title: '未登录',
+					content: '未登录，请登录或注册',
+					showCancel: false,
+					confirmColor: '#3CC51F',
+					success: function() {
+						// 获取当前页面路径
+						const pages = getCurrentPages();
+						const route = pages[pages.length - 1].route;
+						const fullPath = pages[pages.length - 1].$route.fullPath;
+						console.log(route);
+						console.log(fullPath);
+						// 设置跳转路由
+						uni.setStorageSync('redirectUrl', fullPath);
+						uni.setStorageSync('redirectRoute', route);
+						uni.reLaunch({
+							url: '/pages/login/login'
+						});
+					}
+				});
 			}
-		},
-		// 获取当前页面路径并跳转去登录
-		toLogin() {
-			uni.showModal({
-				title: '未登录',
-				content: '未登录，请登录或注册',
-				showCancel: false,
-				confirmColor: '#3CC51F',
-				success: function() {
-					// 获取当前页面路径
-					const pages = getCurrentPages();
-					const route = pages[pages.length - 1].route;
-					const fullPath = pages[pages.length - 1].$route.fullPath;
-					console.log(route);
-					console.log(fullPath);
-					// 设置跳转路由
-					uni.setStorageSync('redirectUrl', fullPath);
-					uni.setStorageSync('redirectRoute', route);
-					uni.reLaunch({
-						url: '/pages/login/login'
-					});
-				}
-			});
 		}
 	}
 </script>
